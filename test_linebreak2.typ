ff4dcb2d07647b35fb1fab2c3365618a902a2ce6
Nicolás Pavez <nicolaspavez@iMac-de-Nicolas.local>
2026-09-12 08:25:22 -0300

    chore: Remove temporary typst test files from root

--- test_linebreak2.typ
+++
@@ -1,1 +0,0 @@
-Hello #linebreak(); (world)
--- test_split.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('ALMADEN_TYPST_TESTING', true);
-require_once 'includes/pdf-typst/page-templates/page-template-word-flow.php';
-$text = "Hola mundo. Esto es una prueba.";
-$res = almaden_bookster_typst_page_template_split_body_at_word($text, 3);
-var_dump($res);
